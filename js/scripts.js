db307fdff9bd9b972eefce284550b1df35849d57
Hyerim Kim <rimmy00902@gmail.com>
2022-02-10 19:45:24 +0900

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,114 @@
+/*
+   
+   Table Of Content
+   
+   1. Global Style
+   2. Preloader (생략)
+   3. Home Section and Navigation Menu
+   4. About
+   5. Skills
+   6. Portfolio
+   7. Contact Us
+   8. Footer
+   9. Scroll To Top
+ 
+
+*/
+
+
+(function ($) {
+    'use strict';
+
+    jQuery(document).ready(function () {
+
+        
+       /* Preloader */
+		
+        $(window).load(function () {
+            $('.preloader').delay(800).fadeOut('slow');
+        });
+		
+		
+		
+       /* Smooth Scroll */
+
+        $('a.smoth-scroll').on("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+		
+
+
+       
+       /* Scroll Naviagation Background Change with Sticky Navigation */
+		 
+        $(window).on('scroll', function () {
+            if ($(window).scrollTop() > 100) {
+                $('.header-top-area').addClass('navigation-background');
+            } else {
+                $('.header-top-area').removeClass('navigation-background');
+            }
+        });
+		
+		
+		
+		
+       /* Mobile Navigation Hide or Collapse on Click */
+		
+        $(document).on('click', '.navbar-collapse.in', function (e) {
+            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+                $(this).collapse('hide');
+            }
+        });
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        
+		 });
+		 
+		
+		
+		
+        /* Scroll To Top */
+		
+        $(window).scroll(function(){
+        if ($(this).scrollTop() >= 500) {
+            $('.scroll-to-top').fadeIn();
+         } else {
+            $('.scroll-to-top').fadeOut();
+         }
+	   });
+	
+	
+	    $('.scroll-to-top').click(function(){
+		  $('html, body').animate({scrollTop : 0},800);
+		  return false;
+	    });
+		
+
+        
+        /* Tooltip */
+         
+         $(function () {
+           $('[data-toggle="tooltip"]').tooltip()
+           })
+		   
+		 
+        
+        /* Ajaxchimp for Subscribe Form */
+		
+         $('#mc-form').ajaxChimp();
+		   
+		   
+  
+		
+        /* Portfolio Filtering */
+
+        $('.portfolio-inner').mixItUp();
+ 
+            });
+
+   })(jQuery);
